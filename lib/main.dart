77061b814e63826794e5c83f9e785059f1703e6e
MiguelG <miguel@tailorbird.us>
2024-03-04 20:18:10 -0500

    splash screen setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mobile/modules/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(App());
 }
 
